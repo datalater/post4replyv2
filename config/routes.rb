ca41595823b8f2693a9727747888a7e611ac304a
datalater <the7mincheol@gmail.com>
2016-05-04 01:47:06 +0000

    heroku_ver01

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   post '/home/write'
   post '/home/reply_write'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
