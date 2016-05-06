1238f7d65e830640319ea51243398cb3c17c9413
datalater <the7mincheol@gmail.com>
2016-05-06 06:43:49 +0000

    wordcount

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,26 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order("id desc")
   end
   
   def reply_write
-    reply = Reply.new
-    reply.content = params[:content]
-    reply.post_id = params[:id_of_post]
-    reply.save
-    
-    redirect_to "/home/index"
+      reply = Reply.new
+      reply.content = params[:content]
+      reply.post_id = params[:id_of_post]
+      reply.save
+      
+      redirect_to "/home/index"
   end
   
   def write
-      post = Post.new
-      post.title = params[:title]
-      post.content = params[:content]
-      post.save
       
-      redirect_to "/home/index"
+      post = Post.new(title: params[:title], content: params[:content])
+      
+      if post.save
+        redirect_to "/home/index"
+      else
+        render :text => post.errors.messages[:title].first
+      end
   end
     
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Post.create(title: '안녕하세요.', content: "첫 글입니다.")
+# Reply.create(content: '첫 리플입니다.', :post_id => 1)
+
+new_post = Post.new
+new_post.title = "[seed :title]"
+new_post.content = "[seed :content]"
+new_post.save
+
+reply = Reply.new
+reply.content ="[seed :reply]"
+reply.post = new_post
+reply.save
